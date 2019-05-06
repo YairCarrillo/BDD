adb3c252b6d105568672ec68bfe37c4b2d355fd2
YairCarrillo <yirz.carrillo@gmail.com>
2019-05-06 08:58:58 -0500

    Update

---
+++ Proyecto/src/proyecto/ManejadorMysql.java
@@ -0,0 +1,54 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package proyecto;
+import java.sql.Array;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author Yirz
+ */
+public class ManejadorMysql {
+    Connection connection;
+    String driver="com.mysql.jdbc.Driver";
+    String user="root";
+    String pw="root";
+    String url="jdbc:mysql://127.0.0.1:3306/galardon_galardonados";
+    public ManejadorMysql(){   
+       this.Connection();
+    }
+    public void Connection(){
+         connection=null;
+        try{
+            Class.forName(driver);
+            connection=DriverManager.getConnection(url, user, pw);
+            if(connection!=null){
+                System.out.println("Coneccion establecida");
+            }
+        }
+        catch(ClassNotFoundException | SQLException e){
+            //e.printStackTrace();
+            System.out.println("error al conectar:"+e);
+        }
+    }
+    public Connection getConnection(){
+        return connection;
+    }
+    public void CloseConnection() throws SQLException{
+        connection.close();
+    }
+    public ResultSet QueryRead(String query) throws SQLException{
+        Statement s= connection.createStatement();
+        ResultSet rs=s.executeQuery(query);
+        return rs;
+    }
+}
---
+++ Proyecto/src/proyecto/PredicadoSimple.java
@@ -0,0 +1,59 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package proyecto;
+
+/**
+ *
+ * @author Yirz
+ */
+public class PredicadoSimple {
+    private String Atributo;
+    private String Operador;
+    private String valor;
+    private String Relacion;
+    public PredicadoSimple(String Atributo,String Operador,String valor,String Relacion){
+        this.Atributo=Atributo;
+        this.Operador=Operador;
+        this.valor=valor;
+        this.Relacion=Relacion;
+    }
+    public String toString(){
+        return Atributo+""+Operador+""+valor+"("+Relacion+")";
+    }
+
+    public String getAtributo() {
+        return Atributo;
+    }
+
+    public String getOperador() {
+        return Operador;
+    }
+
+    public String getValor() {
+        return valor;
+    }
+
+    public String getRelacion() {
+        return Relacion;
+    }
+
+    public void setAtributo(String Atributo) {
+        this.Atributo = Atributo;
+    }
+
+    public void setOperador(String Operador) {
+        this.Operador = Operador;
+    }
+
+    public void setValor(String valor) {
+        this.valor = valor;
+    }
+
+    public void setRelacion(String Relacion) {
+        this.Relacion = Relacion;
+    }
+    
+}
